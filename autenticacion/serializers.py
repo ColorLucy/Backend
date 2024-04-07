ec915436d8ef88b569ea0a2b566d2cec9a53713d
Juan2040751 <viafara.juan@correounivalle.edu.co>
2024-04-07 12:36:23 -0500

    CLW-9 CLW-21 registro

---
+++ autenticacion/serializers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import UserData
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
+    username = serializers.CharField(read_only=True)
+    password = serializers.CharField(write_only=True)
+    is_staff = serializers.BooleanField(default=False)
+
+    class Meta:
+        model = User
+        fields = ["*"]
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserData
+        fields = ["id", "email", "name", "password"]
+
+    def create(self, validated_data):
+        user = UserData.objects.create(email=validated_data['email'],
+                                       name=validated_data['name']
+                                         )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- backend/settings.py
+++ backend/settings.py
@@ -1,8 +1,12 @@
-from pathlib import Path
-from datetime import timedelta
-from corsheaders import middleware
-import environ
 import os
+from datetime import timedelta
+from pathlib import Path
+import dj_database_url
+import environ
+from corsheaders import middleware
+from dotenv import load_dotenv
+load_dotenv()
+
 
 """
 env = environ.Env(
@@ -19,102 +23,49 @@ SECRET_KEY = 'django-insecure-%2$vr3yiaj%yr!x#g_j4%h^o%$xyfd_7$atdfgnb48&jgzc2^%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "0.0.0.0",
-    "127.0.0.1",
-    "*"
-]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_filters',
-    'corsheaders',
-    'rest_framework',
-    'administrador',
-    'productos',
-    'usuario',
-    'compras',
-    'informacion',
-
-]
+    # 'django.contrib.admin',
+    'django.contrib.auth', 'django.contrib.contenttypes',
+    'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles', 'django_filters',
+    'corsheaders', 'rest_framework', 'administrador', 'productos', 'usuario',
+    'compras', 'informacion', 'autenticacion', ]
 
 # implementar autenticación
 
-REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
-}
+REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+                  'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',),
+                  'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication', ], }
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-]
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware',
+              'django.middleware.common.CommonMiddleware', 'django.middleware.csrf.CsrfViewMiddleware',
+              'django.contrib.auth.middleware.AuthenticationMiddleware',
+              'django.contrib.messages.middleware.MessageMiddleware',
+              'django.middleware.clickjacking.XFrameOptionsMiddleware', 'corsheaders.middleware.CorsMiddleware', ]
 
 ROOT_URLCONF = 'backend.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates', 'DIRS': [], 'APP_DIRS': True, 'OPTIONS': {
+    'context_processors': ['django.template.context_processors.debug', 'django.template.context_processors.request',
+                           'django.contrib.auth.context_processors.auth',
+                           'django.contrib.messages.context_processors.messages', ], }, }, ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 # luego se implementan variables de entorno
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'kdHbDULooiwaZjlbxIqXqvsFojUWCMyk',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '27498',
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+AUTH_PASSWORD_VALIDATORS = [{'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
+                            {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
+                            {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
+                            {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', }, ]
+AUTH_USER_MODEL = 'autenticacion.UserData'
 
 """
 CORS_ORIGIN_ALLOW_ALL = True
@@ -141,6 +92,3 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-
-
