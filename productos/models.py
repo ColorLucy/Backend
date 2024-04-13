4fc828717f1b13f2edffb4ed631fdbf4f16b6a97
anvorja <borja.carlos@correounivalle.edu.co>
2024-04-12 23:47:06 -0500

    database normalization

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    id_categoria = models.IntegerField(primary_key=True, unique=True)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    id_producto = models.IntegerField(primary_key=True, unique=True)
+    fabricante = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True)
+    id_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, db_column="id_categoria")
+
+    def __str__(self):
+        return self.descripcion
+
+
+class Detalle(models.Model):
+    id_detalle = models.IntegerField(primary_key=True, unique=True)
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    unidad = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    id_producto = models.ForeignKey(Producto, on_delete=models.CASCADE, db_column="id_producto")
+
+    def __str__(self):
+        return self.nombre
