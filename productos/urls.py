836fa22da8731dd7cf6d640d2a3a0731b2ec373e
anvorja <borja.carlos@correounivalle.edu.co>
2024-04-20 15:13:18 -0500

    models and serializers update

--- productos/urls.py
+++ productos/urls.py
@@ -4,16 +4,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import *
 from .views_crud import *
 
-# 1
-# # urlpatterns = [
-# #     path('productos/', views.ProductoListView.as_view(), name='producto-list'),
-# #     path('productos/<int:pk>/', views.ProductoDetailView.as_view(), name='producto-detail'),
-# #     path('productos/<int:pk>/update/', views.ProductoUpdateView.as_view(), name='producto-update'),
-# #     path('productos/<int:pk>/delete/', views.ProductoDeleteView.as_view(), name='producto-delete'),
-# #     path('productos/create/', views.ProductoCreateView.as_view(), name='producto-create'),
-# # ]
-
-
 urlpatterns = [
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("create-category/", CategoryCreateAPIView.as_view(), name="create_category"),
@@ -30,12 +20,11 @@ urlpatterns = [
     path("view-details/", DetailListAPIView.as_view(), name="view_details"),
     path("rud-detail/", RudDetailAPIView.as_view(), name="rud_detail"),
 
-    
-    path("view-details-products/", DetalleProductoApi.as_view(), name="view_details_products"),
+    path("view-details-products/", DetalleProductoAPIView.as_view(), name="view_details_products"),
 
     path(
         # Experimental
-       "view-image-details/",
+        "view-image-details/",
         DetailImageListAPIView.as_view(),
         name="view_image_details",
     ),
@@ -46,7 +35,8 @@ urlpatterns = [
         name="view_sliced_details",
     ),
 
-    path('detalles-por-categoria/<int:categoria_id>/', DetallesPorCategoriaAPIView.as_view(), name='detalles_por_categoria'),
-
+    path('detalles-por-categoria/<int:categoria_id>/', DetallesPorCategoriaAPIView.as_view(),
+         name='detalles_por_categoria'),
 
 ]
+
