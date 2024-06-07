3510eb42e655da31d06da28cc3d4afe3a9fa30b4
Velyxius <dadawdasd@hotmail.com>
2024-06-06 21:07:26 -0500

    CLW-110: Added Patch Method for Order Status

--- compras/views.py
+++ compras/views.py
@@ -14,6 +14,7 @@ from django.db import transaction
 
 logger = logging.getLogger(__name__)
 
+
 def enviar_notificacion_whatsapp(body):
     try:
         client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
@@ -27,6 +28,7 @@ def enviar_notificacion_whatsapp(body):
         logger.error(f"Twilio error: {e}")
         return None
 
+
 class PedidoAPIView(APIView):
     def get(self, request, *args, **kwargs):
         pedido_id = kwargs.get("pedido_id")
@@ -34,12 +36,28 @@ class PedidoAPIView(APIView):
         serializer = PedidoSerializer(pedido, many=False)
         return Response(serializer.data)
 
+    def patch(self, request, *args, **kwargs):
+        order_id = kwargs.get("pedido_id")
+        order_instance = get_object_or_404(Pedido, pk=order_id)
+
+        new_order_data = request.data
+        serializer = PedidoPatchSerializer(
+            instance=order_instance, data=new_order_data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "Order updated successfully"}, status=status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class PedidosAPIView(APIView):
     def get(self, request):
-        pedidos = Pedido.objects.all().order_by('-fecha_pedido')
+        pedidos = Pedido.objects.all().order_by("-fecha_pedido")
         serializer = PedidoSerializer(pedidos, many=True)
         return Response(serializer.data)
-    
+
     @transaction.atomic
     def post(self, request):
         serializer = PedidoSerializer(data=request.data)
@@ -54,24 +72,22 @@ class PedidosAPIView(APIView):
             # Enviar notificación al cliente
             channel_layer = get_channel_layer()
             async_to_sync(channel_layer.group_send)(
-                "pedidos",
-                {
-                    'type': 'pedido_message',
-                    'message': message
-                }
+                "pedidos", {"type": "pedido_message", "message": message}
             )
-            return Response(serializer.data, status=status.HTTP_201_CREATED)      
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class NotificationListView(APIView):
     def get(self, request):
-        notifications = Notification.objects.all().order_by('-created_at')
+        notifications = Notification.objects.all().order_by("-created_at")
         serializer = NotificationSerializer(notifications, many=True)
         return Response(serializer.data)
 
+
 class PedidosClienteView(APIView):
     def get(self, request):
-        client_id = request.query_params.get('client_id')
+        client_id = request.query_params.get("client_id")
         pedidos_cliente = Pedido.objects.filter(user=client_id)
         serializer_pedido = PedidoSerializer(pedidos_cliente, many=True)
         return Response(serializer_pedido.data, status=status.HTTP_200_OK)
